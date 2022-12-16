6418f7ac2ac854440240ad144fa62a8af04d9713
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-12-16 10:38:47 -0300

    aaaa
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -1,11 +1,17 @@
+#![allow(dead_code, unused)]
 use std::{cmp, collections::HashSet, fs::File, io::Read, str::FromStr};
 
 fn main() {
-    let mut f = File::open("input.txt").unwrap();
+    println!("example 01: {}", ex01(file_to_str("./src/input1.txt")));
+    println!("problem 01: {}", ex01(file_to_str("input.txt")));
+    println!("problem 02: {}", ex02(file_to_str("input.txt")));
+}
+
+fn file_to_str(path: &str) -> String {
+    let mut f = File::open(path).unwrap();
     let mut buf = String::new();
-    f.read_to_string(&mut buf).expect("works");
-    println!("ex01: {}", ex01(buf.clone()));
-    println!("ex02: {}", ex02(buf.clone()));
+    f.read_to_string(&mut buf).expect("yep");
+    buf
 }
 
 #[derive(Debug)]
@@ -36,6 +42,12 @@ struct Movement {
     count: usize,
 }
 
+impl Movement {
+    fn new(dir: Direction, count: usize) -> Self {
+        Self { dir, count }
+    }
+}
+
 fn parse(input: &String) -> Vec<Movement> {
     return input
         .lines()
@@ -50,48 +62,48 @@ fn parse(input: &String) -> Vec<Movement> {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct Position {
+struct Point {
     x: isize,
     y: isize,
 }
 
-impl Position {
-    fn new(x: isize, y: isize) -> Position {
-        Position {
+impl Point {
+    fn new(x: isize, y: isize) -> Point {
+        Point {
             x: cmp::max(x, 0),
             y: cmp::max(y, 0),
         }
     }
 
-    fn chebychev_distance_to(&self, other: &Position) -> isize {
+    fn chebychev_distance_to(&self, other: &Point) -> isize {
         // f64::sqrt((self.x as f64 - pos.x as f64).powi(2) + (self.y as f64 - pos.y as f64).powi(2))
         cmp::max((self.x - other.x).abs(), (self.y - other.y).abs())
     }
 
-    fn is_close_to(&self, pos: &Position) -> bool {
+    fn is_close_to(&self, pos: &Point) -> bool {
         self.chebychev_distance_to(pos) <= 1
     }
 
     fn neighbour(&self, dir: &Direction) -> Self {
         match dir {
-            Direction::Right => Position::new(self.x + 1, self.y),
-            Direction::Left => Position::new(self.x - 1, self.y),
-            Direction::Up => Position::new(self.x, self.y + 1),
-            Direction::Down => Position::new(self.x, self.y - 1),
+            Direction::Right => Point::new(self.x + 1, self.y),
+            Direction::Left => Point::new(self.x - 1, self.y),
+            Direction::Up => Point::new(self.x, self.y + 1),
+            Direction::Down => Point::new(self.x, self.y - 1),
         }
     }
 
     fn new_tail(head: &Self, dir: &Direction) -> Self {
         match dir {
-            Direction::Right => Position::new(head.x - 1, head.y),
-            Direction::Left => Position::new(head.x + 1, head.y),
-            Direction::Up => Position::new(head.x, head.y - 1),
-            Direction::Down => Position::new(head.x, head.y + 1),
+            Direction::Right => Point::new(head.x - 1, head.y),
+            Direction::Left => Point::new(head.x + 1, head.y),
+            Direction::Up => Point::new(head.x, head.y - 1),
+            Direction::Down => Point::new(head.x, head.y + 1),
         }
     }
 }
 
-impl ToString for Position {
+impl ToString for Point {
     fn to_string(&self) -> String {
         format!("{}x{}", self.x, self.y)
     }
@@ -101,65 +113,65 @@ fn ex02(input: String) -> usize {
     0
 }
 
+fn execute(m: &Movement, head: &Point, tail: &Point) -> (Point, Point) {
+    let head = head.neighbour(&m.dir);
+    if !tail.is_close_to(&head) {
+        return (head, Point::new_tail(&head, &m.dir));
+    }
+    (head, tail.clone())
+}
+
 fn ex01(input: String) -> usize {
-    let mut touched: HashSet<Position> = HashSet::new();
+    let mut touched: HashSet<Point> = HashSet::new();
     let mut max_x = 0;
     let mut max_y = 0;
-    let mut head = Position::new(0, 0);
-    let mut tail = Position::new(0, 0);
-    parse(&input).iter().for_each(|m| {
+    let mut head = Point::new(0, 0);
+    let mut tail = Point::new(0, 0);
+    for m in parse(&input).iter() {
         for _ in 0..m.count {
-            // let new_head = match m.dir {
-            //     Direction::Right => Position::new(head.x + 1, head.y),
-            //     Direction::Left => Position::new(head.x - 1, head.y),
-            //     Direction::Up => Position::new(head.x, head.y + 1),
-            //     Direction::Down => Position::new(head.x, head.y - 1),
-            // };
-
-            head = head.neighbour(&m.dir);
+            (head, tail) = execute(m, &head, &tail);
+            touched.insert(tail.clone());
             max_x = cmp::max(head.x, max_x);
             max_y = cmp::max(head.y, max_y);
-            if !tail.is_close_to(&head) {
-                tail = Position::new_tail(&head, &m.dir);
-                // tail = Position::new(
-                //     tail.x + cmp::max(cmp::min(head.x - tail.x, 1), -1),
-                //     tail.y + cmp::max(cmp::min(head.y - tail.y, 1), -1),
-                // );
-            }
-            touched.insert(tail.clone());
-
             // println!("h:{} t:{}", head.to_string(), tail.to_string());
         }
-    });
+    }
 
     println!();
-    dbg!(touched.to_owned());
-    dbg!(max_x);
-    dbg!(max_y);
-    for y in 0..max_y + 1 {
+    // dbg!(touched.to_owned());
+    // dbg!(max_x);
+    // dbg!(max_y);
+    print_matrix(max_x, max_y, head, tail, touched.clone());
+
+    touched.len()
+}
+
+fn print_matrix(max_x: isize, max_y: isize, head: Point, tail: Point, touched: HashSet<Point>) {
+    println!();
+    for y in (0..max_y + 1).rev() {
         println!();
         for x in 0..max_x + 1 {
-            let pos = Position::new(x, y);
+            let pos = Point::new(x, y);
             if head == pos {
                 print!("H");
             } else if tail == pos {
                 print!("T");
             } else if touched.contains(&pos) {
-                print!("o");
+                print!("#");
             } else {
                 print!(".")
             }
         }
     }
     println!();
-    touched.len()
+    println!();
 }
 
 #[cfg(test)]
 mod test {
     use crate::*;
 
-    #[test]
+    // #[test]
     fn input_real() {
         let input = include_str!("../input.txt");
         let res = ex01(input.to_string());
@@ -178,9 +190,9 @@ mod test {
     #[test]
     fn is_pos_close() {
         vec![
-            vec![Position::new(1, 1), Position::new(0, 0)],
-            vec![Position::new(3, 1), Position::new(4, 1)],
-            vec![Position::new(3, 0), Position::new(2, 0)],
+            vec![Point::new(1, 1), Point::new(0, 0)],
+            vec![Point::new(3, 1), Point::new(4, 1)],
+            vec![Point::new(3, 0), Point::new(2, 0)],
         ]
         .iter()
         .for_each(|item| {
@@ -205,9 +217,9 @@ mod test {
     #[test]
     fn is_pos_not_close() {
         vec![
-            vec![Position::new(2, 1), Position::new(0, 0)],
-            vec![Position::new(2, 0), Position::new(4, 1)],
-            vec![Position::new(0, 0), Position::new(2, 0)],
+            vec![Point::new(2, 1), Point::new(0, 0)],
+            vec![Point::new(2, 0), Point::new(4, 1)],
+            vec![Point::new(0, 0), Point::new(2, 0)],
         ]
         .iter()
         .for_each(|item| {
@@ -221,5 +233,105 @@ mod test {
                 p2.to_string()
             );
         });
+    }
+
+    #[test]
+    fn tail_diag_two_steps() {
+        let mut head = Point::new(2, 2);
+        let mut tail = Point::new(1, 1);
+        let m = Movement::new(Direction::Up, 1);
+        (head, tail) = execute(&m, &head, &tail);
+        assert_eq!(
+            Point::new(2, 3),
+            head,
+            "should be in 2,3, is {}",
+            head.to_string()
+        );
+        assert_eq!(
+            Point::new(2, 2),
+            tail,
+            "should be in 2,2, is {}",
+            tail.to_string()
+        );
+    }
+
+    #[test]
+    fn tail_hor_two_steps_rigth() {
+        let mut head = Point::new(2, 1);
+        let mut tail = Point::new(1, 1);
+        let m = Movement::new(Direction::Right, 1);
+        (head, tail) = execute(&m, &head, &tail);
+        assert_eq!(
+            Point::new(3, 1),
+            head,
+            "should be in 3,1, is {}",
+            head.to_string()
+        );
+        assert_eq!(
+            Point::new(2, 1),
+            tail,
+            "should be in 2,1, is {}",
+            tail.to_string()
+        );
+    }
+
+    #[test]
+    fn tail_hor_two_steps_left() {
+        let mut head = Point::new(1, 1);
+        let mut tail = Point::new(2, 1);
+        let m = Movement::new(Direction::Left, 1);
+        (head, tail) = execute(&m, &head, &tail);
+        assert_eq!(
+            Point::new(0, 1),
+            head,
+            "should be in 0,1, is {}",
+            head.to_string()
+        );
+        assert_eq!(
+            Point::new(1, 1),
+            tail,
+            "should be in 1,1, is {}",
+            tail.to_string()
+        );
+    }
+
+    #[test]
+    fn tail_hor_vert_steps_up() {
+        let mut head = Point::new(1, 2);
+        let mut tail = Point::new(1, 1);
+        let m = Movement::new(Direction::Up, 1);
+        (head, tail) = execute(&m, &head, &tail);
+        assert_eq!(
+            Point::new(1, 3),
+            head,
+            "should be in 1,3, is {}",
+            head.to_string()
+        );
+        assert_eq!(
+            Point::new(1, 2),
+            tail,
+            "should be in 1,2, is {}",
+            tail.to_string()
+        );
+    }
+
+    #[test]
+    fn tail_hor_vert_steps_down() {
+        let mut head = Point::new(1, 1);
+        let mut tail = Point::new(1, 2);
+        let m = Movement::new(Direction::Down, 1);
+        (head, tail) = execute(&m, &head, &tail);
+        assert_eq!(
+            Point::new(1, 0),
+            head,
+            "should be in 1,0, is {}",
+            head.to_string()
+        );
+        assert_eq!(
+            Point::new(1, 1),
+            tail,
+            "should be in 1,1, is {}",
+            tail.to_string()
+        );
     }
 }
