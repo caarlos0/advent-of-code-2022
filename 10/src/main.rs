d4f1c5438040f4e202df4f773ef27312ec9c64c3
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-12-19 01:54:43 -0300

    day 10 part 2
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -18,10 +18,10 @@ fn main() {
     );
     println!(
         "example 02: {}",
-        ex02(include_str!("input2.txt").to_string())
+        ex02(include_str!("input1.txt").to_string())
     );
     println!(
-        "problem 02: {}",
+        "problem 02:\n{}",
         ex02(include_str!("input.txt").to_string())
     );
 }
@@ -38,17 +38,25 @@ fn ex01(input: String) -> isize {
     cpu.calc()
 }
 
-fn ex02(input: String) -> usize {
-    unimplemented!()
+fn ex02(input: String) -> String {
+    let mut cpu = CPU::new();
+    input
+        .lines()
+        .into_iter()
+        .map(|l| Op::from_str(l).unwrap())
+        .for_each(|op| {
+            cpu.apply(op);
+        });
+    cpu.draw()
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 enum Type {
     Noop,
     Addx,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Op {
     kind: Type,
     x: isize,
@@ -86,21 +94,45 @@ impl FromStr for Op {
 
 struct CPU {
     cycles: Vec<isize>,
+    screen: Vec<Vec<char>>,
 }
 
 impl CPU {
     fn new() -> Self {
-        Self { cycles: vec![1] }
+        Self {
+            cycles: vec![1],
+            screen: vec![vec!['.'; 40]; 6],
+        }
     }
 
     fn apply(&mut self, op: Op) {
         match op.kind {
-            Type::Noop => self.cycles.push(self.x()),
+            Type::Noop => self.push(self.x()),
             Type::Addx => {
-                self.cycles.push(self.x());
-                self.cycles.push(op.x + self.x());
+                self.push(self.x());
+                self.push(op.x + self.x());
             }
         }
+    }
+
+    fn push(&mut self, x: isize) {
+        let page = self.cycles.len() / 40;
+        let i = self.cycles.len() - 40 * page;
+        let n = isize::try_from(i).unwrap();
+        // print!(
+        //     "Sprite pos from {} to {}, x is {}",
+        //     self.x(),
+        //     self.x() + 2,
+        //     n
+        // );
+        if (self.x()..self.x() + 3).contains(&n) {
+            // print!(" - drawn!");
+            if i != 0 {
+                self.screen[page][i - 1] = '#';
+            }
+        }
+        // println!();
+        self.cycles.push(x);
     }
 
     fn x(&self) -> isize {
@@ -122,6 +154,13 @@ impl CPU {
             .iter()
             .map(|&i| self.signal_at(i))
             .sum()
+    }
+
+    fn draw(&self) -> String {
+        self.screen
+            .iter()
+            .map(|line| line.into_iter().cloned().collect::<String>() + "\n")
+            .collect()
     }
 }
 
@@ -189,5 +228,65 @@ mod test {
         assert_eq!(3960, cpu.signal_at(220));
 
         assert_eq!(13140, cpu.calc());
+    }
+
+    #[test]
+    fn draw() {
+        // TODO: there's still a bug somewhere and the output is not perfect... it is readable
+        // though
+        let expected = vec![
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######.....",
+        ]
+        .join("\n");
+
+        println!("aaaaaaa {}", 57 / 40);
+
+        let mut cpu = CPU::new();
+        include_str!("input2.txt")
+            .lines()
+            .into_iter()
+            .map(|l| Op::from_str(l).unwrap())
+            .for_each(|op| {
+                cpu.apply(op);
+            });
+        println!("{}", cpu.draw());
+        assert_eq!(expected, cpu.draw());
+    }
+
+    #[test]
+    fn push() {
+        let mut cpu = CPU::new();
+        vec![
+            Op::addx(15),
+            Op::addx(-11),
+            Op::addx(6),
+            Op::addx(-3),
+            Op::addx(5),
+            Op::addx(-1),
+            Op::addx(-8),
+            Op::addx(13),
+            Op::addx(4),
+            Op::noop(),
+            Op::addx(-1),
+        ]
+        .iter()
+        .for_each(|op| cpu.apply(op.to_owned()));
+        println!("{:?}", cpu.screen[0]);
+        "##..##..##..##..##..#"
+            .chars()
+            .into_iter()
+            .enumerate()
+            .for_each(|(i, p)| {
+                assert_eq!(
+                    p, cpu.screen[0][i],
+                    "expected char at {} to be {}, was {}",
+                    i, p, cpu.screen[0][i]
+                );
+            });
     }
 }
